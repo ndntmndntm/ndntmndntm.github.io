0bfe3b39b9ec7c4ed926e19f4ba59b777803f990
ndntmndntm <solbonricnhinoff@gmail.com>
2025-08-26 01:16:19 +0200

    gigatest

--- js/mrt.js
+++ js/mrt.js
@@ -1,11 +1,90 @@
-let names_textarea = document.getElementById("mrt_names");
+let names_list = document.getElementById("mrt_names");
 let note_textarea = document.getElementById("mrt_note");
 let result_textarea = document.getElementById("mrt_result");
 
+let mrt_states = {};
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function updatePage() {
+    names_list.innerHTML = "";
+    saveMRTStates();
+    if (mrt_states != {} && mrt_states["players"] != {}) {
+        for (const [k, v] of Object.entries(mrt_states["players"])) {
+            updateRow(k, v);
+        }
+    }
+}
+
+function updateRow(k, v) {
+    let item = document.createElement("div");
+    item.setAttribute("class", "wacopy-div");
+    item.appendChild(makeNameText(k, v));
+    item.appendChild(makeSpecText(k, v));
+    item.appendChild(makeRemoveButton(k, v));
+
+    let wrapper = document.createElement("li");
+    wrapper.appendChild(item);
+
+    names_list.appendChild(wrapper);
+}
+
+
+function makeRemoveButton(k, v) {
+    let button = document.createElement("button");
+    button.setAttribute("class", "gigabutton");
+    button.onclick = () => {
+        delete mrt_states["players"][k];
+        updatePage();
+    };
+    button.innerHTML = "<span>Delete</span>";
+    return button;
+}
+
+function makeNameText(k, v) {
+    let p = document.createElement("p");
+    p.innerHTML = v["name"];
+    p.onchange = (event) => v["name"] = event.target.value;
+    return p;
+}
+function makeSpecText(k, v) {
+    let p = document.createElement("p");
+    p.innerHTML = v["spec"];
+    p.onchange = (event) => v["spec"] = event.target.value;
+    return p;
+}
+
+function saveMRTStates() {
+    mrt_states["note"] = note_textarea.value;
+    setCookie("mrt", JSON.stringify(mrt_states));
+}
+
 function MRTGenerate() {
-    let names = JSON.parse("" + names_textarea.value);
+    let names = JSON.parse("" + names_list.value);
     let note = note_textarea.value;
 
+    saveMRTStates();
     for (const [key, value] of Object.entries(names)) {
         console.log(key, value);
         note = note.replaceAll(key, value);
@@ -15,4 +94,10 @@ function MRTGenerate() {
 
 function MRTCopy() {
     navigator.clipboard.writeText(result_textarea.textContent);
+}
+
+window.onload = () => {
+    mrt_states = JSON.parse(getCookie("mrt"));
+    names_list.value = mrt_states["players"];
+    note_textarea.value = mrt_states["note"];
 }
