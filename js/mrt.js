f1d938980b28319a00a9f27c1b1fec5ba2c7c663
ndntmndntm <solbonricnhinoff@gmail.com>
2025-08-25 22:42:00 +0200

    test

--- js/mrt.js
+++ js/mrt.js
@@ -3,13 +3,14 @@ let note_textarea = document.getElementById("mrt_note");
 let result_textarea = document.getElementById("mrt_result");
 
 function MRTGenerate() {
-    let names = JSON.parse("" + names_textarea.textContent);
-    let note = note_textarea.textContent;
+    let names = JSON.parse("" + names_textarea.value);
+    let note = note_textarea.value;
 
     for (const [key, value] of Object.entries(names)) {
+        console.log(key, value);
         note = note.replaceAll(key, value);
     }
-    result_textarea.textContent = note;
+    result_textarea.value = note;
 }
 
 function MRTCopy() {
